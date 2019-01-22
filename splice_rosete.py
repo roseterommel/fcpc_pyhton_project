d62100b95ff0ee899e94fea00af9e8dfca7246e9
Rommel Rosete <45374767+roseterommel@users.noreply.github.com>
2019-01-22 11:13:29 +0800

    Splice

---
+++ splice_rosete.py
@@ -0,0 +1,12 @@
+
+listX=[10,11,12,13,14,15,16,17]
+
+print(listX[1:4])
+
+centlist=[0.1,0.2,0.3,0.4,0.5,0.6,]
+
+print(centlist[3:7])
+
+streetfood =["hotdog","kikiam","fishball","quekquek","isaw"]
+
+print(streetfood[1:3])
