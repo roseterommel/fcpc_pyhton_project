35b9037754c8c1ce2963ef048c511099316acd1b
Rommel Rosete <45374767+roseterommel@users.noreply.github.com>
2019-02-12 09:24:37 +0800

    Contacts list

---
+++ Activity_B_rosete.py
@@ -0,0 +1,9 @@
+
+mycontacts={}
+
+while True:
+    u_int= input("Enter name: ")
+    cnumber= input("Enter Number: ")
+    mycontacts ['name']= [u_int]
+    mycontacts['number'] =[cnumber]
+    print(mycontacts)
