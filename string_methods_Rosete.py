21131f4c8926734ec753855dfe3ed76f9912d692
Rommel Rosete <45374767+roseterommel@users.noreply.github.com>
2019-02-19 10:21:11 +0800

    Methods

---
+++ string_methods_Rosete.py
@@ -0,0 +1,20 @@
+print("#1Capitalized")
+word = "hello"
+print(word.capitalize())
+print()
+print("#Islower")
+word = "hi"
+print(word.islower())
+print()
+print("#Center")
+Rommel = "Rommel"
+print("Hello: ",Rommel.center(20))
+print()
+print("#Isnumeric")
+number = "12345"
+print(number.isnumeric())
+print()
+print("#Casefold")
+word = "HELLO WORLD"
+print(word.casefold())
+print
